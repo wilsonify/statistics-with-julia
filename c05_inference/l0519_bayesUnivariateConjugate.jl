6b1b554978dab384c231d0412ce011d2a3413c87
wilsonify <tom.andrew.wilson@gmail.com>
2023-10-22 18:04:35 -0400

    org ch05

--- c05_inference/l0519_bayesUnivariateConjugate.jl
+++ c05_inference/l0519_bayesUnivariateConjugate.jl
@@ -1,21 +1,21 @@
 using Distributions, Plots; pyplot()
 
 alpha, beta = 8, 2
-prior(lam) = pdf(Gamma(alpha, 1/beta), lam)
-data = [2,1,0,0,1,0,2,2,5,2,4,0,3,2,5,0]
+prior(lam) = pdf(Gamma(alpha, 1 / beta), lam)
+data = [2, 1, 0, 0, 1, 0, 2, 2, 5, 2, 4, 0, 3, 2, 5, 0]
 
 like(lam) = *([pdf(Poisson(lam),x) for x in data]...)
-posteriorUpToK(lam) = like(lam)*prior(lam)
+posteriorUpToK(lam) = like(lam) * prior(lam)
 
 delta = 10^-4.
 lamRange = 0:delta:10
-K = sum([posteriorUpToK(lam)*delta for lam in lamRange])
-posterior(lam) = posteriorUpToK(lam)/K
+K = sum([posteriorUpToK(lam) * delta for lam in lamRange])
+posterior(lam) = posteriorUpToK(lam) / K
 
-bayesEstimate = sum([lam*posterior(lam)*delta for lam in lamRange])
+bayesEstimate = sum([lam*posterior(lam) * delta for lam in lamRange])
 
 newAlpha, newBeta = alpha + sum(data), beta + length(data)
-closedFormBayesEstimate = mean(Gamma(newAlpha, 1/newBeta))
+closedFormBayesEstimate = mean(Gamma(newAlpha, 1 / newBeta))
 
 println("Computational Bayes Estimate: ", bayesEstimate)
 println("Closed form Bayes Estimate: ", closedFormBayesEstimate)
