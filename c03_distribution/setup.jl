bedd89c9700859198c4cba0817916ed7467ec181
wilsonify <tom.a.wilson@gmail.com>
2023-10-22 10:36:17 -0400

    add setup for c03

---
+++ c03_distribution/setup.jl
@@ -0,0 +1,16 @@
+using Pkg
+path_to_here = @__DIR__
+Pkg.activate("$path_to_here")
+Pkg.add("HCubature")
+Pkg.add("Plots")
+Pkg.add("StatsBase")
+Pkg.add("Distributions")
+Pkg.add("Statistics")
+Pkg.add("SpecialFunctions")
+Pkg.add("PyPlot")
+Pkg.add("LinearAlgebra")
+Pkg.add("Measures")
+Pkg.add("LaTeXStrings")
+Pkg.add("QuadGK")
+Pkg.add("Calculus")
+Pkg.add("Random")
