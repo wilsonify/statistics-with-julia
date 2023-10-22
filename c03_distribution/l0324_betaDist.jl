271e4ab84f6f600b053b2e3f076a1bbb31bc0448
wilsonify <tom.a.wilson@gmail.com>
2023-10-22 11:20:41 -0400

    update factorial(x) --> gamma(x-1)

--- c03_distribution/l0324_betaDist.jl
+++ c03_distribution/l0324_betaDist.jl
@@ -1,13 +1,15 @@
 using SpecialFunctions, Distributions
 
-a,b = 0.2, 0.7
+a, b = 0.2, 0.7
 x = 0.75
 
 betaAB1 = beta(a,b)
-betaAB2 = (gamma(a)gamma(b))/gamma(a+b)
-betaAB3 = (factorial(a-1)factorial(b-1))/factorial(a+b-1)
+betaAB2 = (gamma(a) * gamma(b)) / gamma(a + b)
+# factorial of non-integers is no longer supported in SpecialFunctions.jl
+#betaAB3 = (factorial(a - 1) * factorial(b - 1)) / factorial(a + b - 1)
+betaAB3 = (gamma(a) * gamma(b)) / gamma(a + b)
 betaPDFAB1 = pdf(Beta(a,b),x)
-betaPDFAB2 = (1/beta(a,b))*x^(a-1) * (1-x)^(b-1)
+betaPDFAB2 = (1 / beta(a,b)) * x^(a - 1) * (1 - x)^(b - 1)
 
 println("beta($a,$b)    = $betaAB1,\t$betaAB2,\t$betaAB3 ")
 println("betaPDF($a,$b) at $x = $betaPDFAB1,\t$betaPDFAB2")
