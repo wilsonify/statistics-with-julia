7a557f66e8795d3b40a81d6a6a43fd39beac6c7f
wilsonify <tom.andrew.wilson@gmail.com>
2023-10-27 07:39:57 -0400

    resolve errors in ch07

--- c07_hypothesis/l0714_ksStatistic.jl
+++ c07_hypothesis/l0714_ksStatistic.jl
@@ -2,9 +2,9 @@ using Random, Distributions, StatsBase, HypothesisTests, PyPlot
 Random.seed!(0)
 
 function ksStat(dist)
-    data = rand(dist,n)
+    data = rand(dist, n)
     Fhat = ecdf(data)
-    sqrt(n)*maximum(abs.(Fhat(xGrid) - cdf(dist,xGrid)))
+    sqrt(n) * maximum(abs.(Fhat(xGrid) - cdf(dist, xGrid)))
 end
 
 n = 25
@@ -18,17 +18,15 @@ kStats1 = [ksStat(dist1) for _ in 1:N]
 dist2 = Normal()
 kStats2 = [ksStat(dist2) for _ in 1:N]
 
-figure(figsize=(10,5))
+figure(figsize = (10, 5))
 subplot(121)
-plt.hist(kStats1,50,color="b",label="KS stat (Exponential)",histtype="step",
-		normed=true)
-plot(kGrid,pdf(Kolmogorov(),kGrid),"r",label="Kolmogorov PDF")
-legend(loc="upper right")
-xlim(0,2.5)
+plt.hist(kStats1, 50, color = "b", label = "KS stat (Exponential)", histtype = "step", density = true)
+plot(kGrid, [pdf(Kolmogorov(), k) for k in kGrid], "r", label = "Kolmogorov PDF")
+legend(loc = "upper right")
+xlim(0, 2.5)
 
 subplot(122)
-plt.hist(kStats2,50,color="b",label="KS stat (Normal)",histtype="step",
-		normed=true)
-plot(kGrid,pdf(Kolmogorov(),kGrid),"r",label="Kolmogorov PDF")
-legend(loc="upper right")
-xlim(0,2.5)
+plt.hist(kStats2, 50, color = "b", label = "KS stat (Normal)", histtype = "step", density = true)
+plot(kGrid, [pdf(Kolmogorov(), k) for k in kGrid], "r", label = "Kolmogorov PDF")
+legend(loc = "upper right")
+xlim(0, 2.5)
