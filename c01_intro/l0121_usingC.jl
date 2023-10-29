b1870d78b543d33b4ca50d365f9596f2d2bd292b
wilsonify <tom.andrew.wilson@gmail.com>
2023-10-29 18:57:58 -0400

    more setup

--- c01_intro/l0121_usingC.jl
+++ c01_intro/l0121_usingC.jl
@@ -1,9 +1,8 @@
 # Julia allows C and Fortran calls to be made directly without adding any extra overhead than a standard library call
 # Note that the code to be called must be available as a shared library.
-using Sys
 
 if Sys.iswindows()
     ccall((:cos, "msvcrt"), Float64, (Float64,), pi )
 else
-    ccall((:cos, "libc"), Float64, (Float64,), pi)
+    result = ccall(:cos, Float64, (Float64,), π)
 end
