dff4365ec597c9f880d780dd8045d06971561862
wilsonify <tom.a.wilson@gmail.com>
2023-10-29 09:10:38 -0400

    add tests for l04

--- tests/test_c01_intro/test_l0104_fast.jl
+++ tests/test_c01_intro/test_l0104_fast.jl
@@ -1,5 +1,5 @@
 # Tests for Fast code example
-using Statistics
+using Statistics, Random
 
 path_to_here = @__DIR__
 path_to_module = abspath("$path_to_here/../../c01_intro")
@@ -9,4 +9,12 @@ include("$path_to_module/l0104_fast.jl")
 @testset "Test comprehension_generate_data function" begin
     result = comprehension_generate_data(100,10)
     @test length(result) ==  100
+end
+
+@testset "Test summarize_data function" begin
+    Random.seed!(0)
+    data = comprehension_generate_data(100,10)
+    result = summarize_data(data)
+    result = round.(result, digits = 2)
+    @test result == (0.31, 0.66)
 end
