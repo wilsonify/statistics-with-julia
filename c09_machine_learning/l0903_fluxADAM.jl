76e47cb52362c6cb440063b62165735c4be7c959
wilsonify <tom.andrew.wilson@gmail.com>
2023-10-29 10:23:16 -0400

    resolve error in ch09

--- c09_machine_learning/l0903_fluxADAM.jl
+++ c09_machine_learning/l0903_fluxADAM.jl
@@ -1,9 +1,9 @@
-using Flux, Random, LinearAlgebra, CSV
+using Flux, Random, LinearAlgebra, CSV, DataFrames
 using Flux.Optimise: update!
 Random.seed!(0)
 path_to_here=@__DIR__
 path_to_data=abspath("$path_to_here/../data")
-data = CSV.read("$path_to_data/L1L2data.csv")
+data = CSV.read("$path_to_data/L1L2data.csv", DataFrame)
 xVals, yVals = Array{Float64}(data.X), Array{Float64}(data.Y)
 eta = 0.05
 epsilon = 10^-7
