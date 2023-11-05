f28799b063b77e12ef117866f34d00e9b894a616
wilsonify <tom.andrew.wilson@gmail.com>
2023-11-05 17:14:54 -0500

    tests for 04 12/13

--- tests/test_c04_data/test_l0412_meanVectCovMatrixSummary.jl
+++ tests/test_c04_data/test_l0412_meanVectCovMatrixSummary.jl
@@ -1,24 +1,97 @@
 using DataFrames, CSV, Statistics
-function main()
-    path_to_here = @__DIR__
-    path_to_data = abspath("$path_to_here/../data")
+path_to_here = @__DIR__
+include("$path_to_here/t01_are_dataframes_equal.jl")
+include("$path_to_here/t02_are_lists_equal.jl")
+include("$path_to_here/t04_dataframe_to_dict.jl")
 
-    data = CSV.read("$path_to_data/temperatures.csv", DataFrame, copycols = true)
+path_to_data = abspath("$path_to_here/../../data")
+read_temperatures() = CSV.read("$path_to_data/temperatures.csv", DataFrame, copycols=true)
+
+function compute_meanVect(data)
     brisT = data.Brisbane
     gcT = data.GoldCoast
+    meanVect = [mean(brisT), mean(gcT)]
+    return meanVect
+end
 
+function compute_covMat(data)
+    brisT = data.Brisbane
+    gcT = data.GoldCoast
     sigB = std(brisT)
     sigG = std(gcT)
-    covBG = cov(brisT, gcT)
-
-    meanVect = [mean(brisT), mean(gcT)]
+    covBG = cov(brisT, gcT)    
     covMat = [
         sigB^2 covBG
         covBG sigG^2
     ]
+    return covMat
+end
 
-    outfile = open("../data/mvParams.jl", "w")
-    write(outfile, "meanVect = $meanVect \ncovMat = $covMat")
+function compute_mvParams(data)
+    mvParams = Dict(
+        "meanVect" => compute_meanVect(data),
+        "covMat" => compute_covMat(data),
+    )
+    return mvParams
+end
+
+function mvParams_to_string(mvParams_dict)
+    return "meanVect = $(mvParams_dict["meanVect"]) \ncovMat = $(mvParams_dict["covMat"])"
+end
+
+function write_string_to_file(string_to_write,output_file_path)
+    outfile = open(output_file_path, "w")
+    write(outfile, string_to_write)
     close(outfile)
-    print(read("$path_to_data/mvParams.jl", String))
+end
+
+
+function read_mvParams(input_file_path)
+    return read(input_file_path, String)
+end
+
+using Test
+@testset "read_temperatures test" begin
+    df = read_temperatures()
+    expected_data = DataFrame(   Dict{Symbol,Array{Any,1}}(:Year => [2015, 2015, 2015],:Day => [1, 2, 3],:GoldCoast => [30.9, 30.1, 30.1],:Brisbane => [31.3, 30.5, 28.9],:Month => [1, 1, 1]))
+    @test dataframe_to_dict(first(df,3))== dataframe_to_dict(expected_data)
+end
+
+
+@testset "compute_meanVect test" begin
+    df = read_temperatures()
+    meanVect = compute_meanVect(df)
+    meanVect = round.(meanVect,digits=2)
+    @test meanVect == [27.16, 26.16]
+end
+
+@testset "compute_covMat test" begin
+    df = read_temperatures()
+    covMat = compute_covMat(df)
+    covMat = round.(covMat,digits=2)
+    @test covMat == [16.13 13.05; 13.05 12.37]
+end
+
+@testset "compute_mvParams test" begin
+    df = read_temperatures()
+    mvParams = compute_mvParams(df)
+    expected_dict=Dict{String,Array{Float64,N} where N}("meanVect" => [27.155405405405407, 26.163835263835264],"covMat" => [16.1253895583728 13.046961200891614; 13.046961200891614 12.367253570765163])
+    @test mvParams == expected_dict
+end
+
+
+@testset "mvParams_to_string test" begin
+    df = read_temperatures()
+    mvParams = compute_mvParams(df)
+    mvParams_str = mvParams_to_string(mvParams)
+    @test mvParams_str == "meanVect = [27.155405405405407, 26.163835263835264] \ncovMat = [16.1253895583728 13.046961200891614; 13.046961200891614 12.367253570765163]"
+end
+
+@testset "write_string_to_file test" begin
+    df = read_temperatures()
+    mvParams = compute_mvParams(df)
+    mvParams_str = mvParams_to_string(mvParams)
+    write_string_to_file(mvParams_str,"$path_to_data/mvParams.txt")
+    read_string_from_file = read_mvParams("$path_to_data/mvParams.txt")
+    @test read_string_from_file == "meanVect = [27.155405405405407, 26.163835263835264] \ncovMat = [16.1253895583728 13.046961200891614; 13.046961200891614 12.367253570765163]"
 end
--- tests/test_c04_data/test_l0413_sampleCovarianceMatrix.jl
+++ tests/test_c04_data/test_l0413_sampleCovarianceMatrix.jl
@@ -1,51 +1,221 @@
 using Statistics, StatsBase, LinearAlgebra, DataFrames, CSV
-function main()
-    path_to_here = @__DIR__
-    path_to_data = abspath("$path_to_here/../data")
+path_to_here = @__DIR__
+include("$path_to_here/t01_are_dataframes_equal.jl")
+include("$path_to_here/t02_are_lists_equal.jl")
+include("$path_to_here/t04_dataframe_to_dict.jl")
 
-    df = CSV.read("$path_to_data/3featureData.csv", DataFrame, header = false)
-    n, p = size(df)
-    println("Number of features: ", p)
-    println("Number of observations: ", n)
-    X = convert(Array{Float64,2}, Matrix(df))
-    println("Dimensions of data matrix: ", size(X))
+path_to_data = abspath("$path_to_here/../../data")
 
-    xbarA = (1 / n) * X' * ones(n)
-    xbarB = [mean(X[:, i]) for i = 1:p]
-    xbarC = sum(X, dims = 1) / n
-    println("\nAlternative calculations of (sample) mean vector: ")
-    @show(xbarA), @show(xbarB), @show(xbarC)
+read_3featureData() = CSV.read("$path_to_data/3featureData.csv", DataFrame, header = false)
 
-    Y = (I - ones(n, n) / n) * X
-    println("Y is the de-meaned data: ", mean(Y, dims = 1))
+function df_to_array_3featureData(data)
+    return convert(Array{Float64,2}, Matrix(data))
+end
 
+function sample_mean_vector1(X)
+     n, p = size(X)
+     return (1 / n) * X' * ones(n)
+     end
+function sample_mean_vector2(X)
+     n, p = size(X)
+     return [mean(X[:, i]) for i = 1:p]
+     end
+
+function sample_mean_vector3(X)
+     n, p = size(X)
+     return sum(X, dims = 1) / n
+     end
+de_mean(X) = (I - ones(n, n) / n) * X
+
+function sample_covariance_matrix1(X)
+    n, p = size(X)
+    xbarA = sample_mean_vector1(X)
     first_term = (X .- xbarA')'
     second_term = (X .- xbarA')
     numerator = first_term * second_term
     denominator = (n - 1)
-    covA = numerator / denominator
-    covB = Y' * Y / (n - 1)
-    covC = [cov(X[:, i], X[:, j]) for i = 1:p, j = 1:p]
-    covD = [cor(X[:, i], X[:, j]) * std(X[:, i]) * std(X[:, j]) for i = 1:p, j = 1:p]
-    covE = cov(X)
-    println("\nAlternative calculations of (sample) covariance matrix: ")
-    @show(covA), @show(covB), @show(covC), @show(covD), @show(covE)
+    cov = numerator / denominator
+    return cov
+end
+function sample_covariance_matrix2(X)
+    n, p = size(X)
+    Y = de_mean(X)
+    cov = Y' * Y / (n - 1)
+    return cov
+end
 
-    ZmatA = [(X[i, j] - mean(X[:, j])) / std(X[:, j]) for i = 1:n, j = 1:p]
-    ZmatB = hcat([zscore(X[:, j]) for j = 1:p]...)
-    println(
-        "\nAlternate computation of Z-scores yields same matrix: ",
-        maximum(norm(ZmatA - ZmatB)),
-    )
+function sample_covariance_matrix3(X)
+    n, p = size(X)
+    return [cov(X[:, i], X[:, j]) for i = 1:p, j = 1:p]
+end
+
+function sample_covariance_matrix4(X)
+    n, p = size(X)
+    return [cor(X[:, i], X[:, j]) * std(X[:, i]) * std(X[:, j]) for i = 1:p, j = 1:p]
+end
+
+
+function sample_covariance_matrix5(X)
+    return cov(X)
+end
+
+
+function Z_scores1(X)
+    return [(X[i, j] - mean(X[:, j])) / std(X[:, j]) for i = 1:n, j = 1:p]
+end
+
+function Z_scores2(X)
+    return hcat([zscore(X[:, j]) for j = 1:p]...)
+end
+
+function sample_correlation_matrix1(X)
+    return covA ./ [std(X[:, i]) * std(X[:, j]) for i = 1:p, j = 1:p]
+end
+
+function sample_correlation_matrix2(X)
+    return covA ./(std(X, dims = 1)' * std(X, dims = 1))
+end
+function sample_correlation_matrix3(X)
+   return [cor(X[:, i], X[:, j]) for i = 1:p, j = 1:p]
+end
+
+function sample_correlation_matrix4(X)
+    return Z' * Z / (n - 1)
+end
+
+function sample_correlation_matrix5(X)
+    return cov(Z)
+end
+
+function sample_correlation_matrix6(X)
+    return cor(X)
+end
+
+using Test
+
+@testset "read_3featureData" begin
+    df = read_3featureData()
+    result = size(df)
+    @test result == (7, 3)
+end
+@testset "df_to_array_3featureData" begin
+    df = read_3featureData()
+    x = df_to_array_3featureData(df)
+    @test size(x) == (7, 3)
+end
+@testset "sample_mean_vector1" begin
+    df = read_3featureData()
+    x = df_to_array_3featureData(df)
+    result = sample_mean_vector1(x)
+    result = round.(result,digits = 2)
+    @test result == [1.06, 2.09, 3.5]
+end
+
+@testset "sample_mean_vector2" begin
+    df = read_3featureData()
+    x = df_to_array_3featureData(df)
+    result = sample_mean_vector2(x)
+    result = round.(result,digits = 2)
+    @test result == [1.06, 2.09, 3.5]
+end
+
+@testset "sample_mean_vector3" begin
+    df = read_3featureData()
+    x = df_to_array_3featureData(df)
+    result = sample_mean_vector3(x)
+    result = round.(result,digits = 2)
+    @test result == [1.06 2.09 3.5]
+end
+
+
+@testset "sample_covariance_matrix1" begin
+    df = read_3featureData()
+    x = df_to_array_3featureData(df)
+    result = sample_covariance_matrix1(x)
+    result = round.(result,digits = 2)
+    @test result == [0.12 -0.09 0.44; -0.09 0.12 -0.71; 0.44 -0.71 8.03]
+end
+
+@testset "sample_covariance_matrix2" begin
+    df = read_3featureData()
+    x = df_to_array_3featureData(df)
+    result = sample_covariance_matrix2(x)
+    result = round.(result,digits = 2)
+    @test result == [0.12 -0.09 0.44; -0.09 0.12 -0.71; 0.44 -0.71 8.03]
+end
+@testset "sample_covariance_matrix3" begin
+    df = read_3featureData()
+    x = df_to_array_3featureData(df)
+    result = sample_covariance_matrix3(x)
+    result = round.(result,digits = 2)
+    @test result == [0.12 -0.09 0.44; -0.09 0.12 -0.71; 0.44 -0.71 8.03]
+end
+@testset "sample_covariance_matrix4" begin
+    df = read_3featureData()
+    x = df_to_array_3featureData(df)
+    result = sample_covariance_matrix4(x)
+    result = round.(result,digits = 2)
+    @test result == [0.12 -0.09 0.44; -0.09 0.12 -0.71; 0.44 -0.71 8.03]
+end
+@testset "sample_covariance_matrix5" begin
+    df = read_3featureData()
+    x = df_to_array_3featureData(df)
+    result = sample_covariance_matrix5(x)
+    result = round.(result,digits = 2)
+    @test result == [0.12 -0.09 0.44; -0.09 0.12 -0.71; 0.44 -0.71 8.03]
+end
+@testset "end-to-end" begin
+    df = read_3featureData()
+    n, p = size(df)
+    println("Number of features: ", p)
+    println("Number of observations: ", n)
+
+    x = df_to_array_3featureData(data)
+    println("Dimensions of data matrix: ", size(x))
+
+    xbarA = sample_mean_vector1(x)
+    xbarB = sample_mean_vector2(x)
+    xbarC = sample_mean_vector3(x)
+    println("\nAlternative calculations of (sample) mean vector: ")
+    @show(xbarA)
+    @show(xbarB)
+    @show(xbarC)
+
+    Y = de_mean(x)
+    println("Y is the de-meaned data: ", mean(Y, dims = 1))
+
+    covA = sample_covariance_matrix1(x)
+    covB = sample_covariance_matrix2(x)
+    covC = sample_covariance_matrix3(x)
+    covD = sample_covariance_matrix4(x)
+    covE = sample_covariance_matrix5(x)
+
+    println("\nAlternative calculations of (sample) covariance matrix: ")
+    @show(covA)
+    @show(covB)
+    @show(covC)
+    @show(covD)
+    @show(covE)
+
+    ZmatA = Z_scores1(x)
+    ZmatB = Z_scores2(x)
+    println( "\nAlternate computation of Z-scores yields same matrix: ", maximum(norm(ZmatA - ZmatB)), )
     Z = ZmatA
 
-    corA = covA ./ [std(X[:, i]) * std(X[:, j]) for i = 1:p, j = 1:p]
-    corB = covA ./ (std(X, dims = 1)' * std(X, dims = 1))
-    corC = [cor(X[:, i], X[:, j]) for i = 1:p, j = 1:p]
-    corD = Z' * Z / (n - 1)
-    corE = cov(Z)
-    corF = cor(X)
+    corA = sample_correlation_matrix1(x)
+    corB = sample_correlation_matrix2(x)
+    corC = sample_correlation_matrix3(x)
+    corD = sample_correlation_matrix4(x)
+    corE = sample_correlation_matrix5(x)
+    corF = sample_correlation_matrix6(x)
     println("\nAlternative calculations of (sample) correlation matrix: ")
-    @show(corA), @show(corB), @show(corC), @show(corD), @show(corE), @show(corF)
+    @show(corA)
+    @show(corB)
+    @show(corC)
+    @show(corD)
+    @show(corE)
+    @show(corF)
 
 end
+
+
