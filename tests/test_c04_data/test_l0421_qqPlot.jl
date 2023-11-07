894e4a67d731dd328b2426853a93717f7b91de3e
wilsonify <tom.a.wilson@gmail.com>
2023-11-07 07:47:32 -0500

    test qqplot

--- tests/test_c04_data/test_l0421_qqPlot.jl
+++ tests/test_c04_data/test_l0421_qqPlot.jl
@@ -1,15 +1,29 @@
 using Random, Distributions, StatsPlots, Plots, Measures;
 pyplot();
-function main()
+
+function generate_beta_data(n,a,b)
+    dist = Beta(a, b)
+    data = rand(dist, n)
+    return data
+end
+
+@testset "generate_beta_data test" begin
     Random.seed!(0)
-
-    b1, b2 = 0.5, 2
-    dist1, dist2, = Beta(b1, b1), Beta(b2, b2)
-
     n = 2000
-    data1 = rand(dist1, n)
-    data2 = rand(dist2, n)
+    b1 = 0.5
+    b2 = 2
+    data1 = generate_beta_data(n,b1,b1)
+    @test length(data1) == n
+    @test typeof(data1) == Vector{Float64}
+end
 
+@testset "plot hist" begin
+    Random.seed!(0)
+    n = 2000
+    b1 = 0.5
+    b2 = 2
+    data1 = generate_beta_data(n,b1,b1)
+    data2 = generate_beta_data(n,b2,b2)
     stephist(data1, bins = 15, label = "beta($b1,$b1)", c = :red, normed = true)
     p1 = stephist!(
         data2,
@@ -20,7 +34,6 @@ function main()
         ylabel = "Density",
         normed = true,
     )
-
     p2 = qqplot(
         data1,
         data2,
@@ -31,6 +44,5 @@ function main()
         ylabel = "Quantiles for beta($b2,$b2) sample",
         legend = false,
     )
-
     plot(p1, p2, size = (800, 400), margin = 5mm)
 end
