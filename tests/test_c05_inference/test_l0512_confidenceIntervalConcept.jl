4ed713c41bcd25cfd672f20ff7dea00c9848ebec
wilsonify <tom.andrew.wilson@gmail.com>
2023-11-12 10:31:39 -0500

    ch05 tests

--- tests/test_c05_inference/test_l0512_confidenceIntervalConcept.jl
+++ tests/test_c05_inference/test_l0512_confidenceIntervalConcept.jl
@@ -1,11 +1,20 @@
 using Random, Distributions
-Random.seed!(0)
-
 alpha = 0.05
+
 L(obs) = obs - (1 - sqrt(alpha))
 U(obs) = obs + (1 - sqrt(alpha))
 
+function generate_data_triangle(mu)
+    sample = rand(TriangularDist(mu - 1,mu + 1,mu))
+    return sample
+end
+
+@testset "end_to_end" begin
+Random.seed!(0)
 mu = 5.57
-observation = rand(TriangularDist(mu - 1,mu + 1,mu))
-println("Lower bound L: ", L(observation))
-println("Upper bound U: ", U(observation))
+observation = generate_data_triangle(mu)
+Lower_bound = round(L(observation),digits = 2)
+Upper_bound = round(U(observation),digits = 2)
+@test Lower_bound == 5.2
+@test Upper_bound == 6.75
+end
--- tests/test_c05_inference/test_l0514_teststatPvalRejectregion.jl
+++ tests/test_c05_inference/test_l0514_teststatPvalRejectregion.jl
@@ -1,30 +1,56 @@
-using Distributions, Random, Statistics, Plots; pyplot()
-Random.seed!(2)
-
-n, N, alpha = 10, 10^7, 0.05
-mActual = 0.75
-dist0, dist1 = Uniform(0,1), Uniform(0,mActual)
+using Distributions, Random, Statistics, Plots
 
 ts(sample) = maximum(sample) - minimum(sample)
 
-empiricalDistUnderH0 = [ts(rand(dist0,n)) for _ in 1:N]
-rejectionValue = quantile(empiricalDistUnderH0,alpha)
+using Test,Random
+@testset "rejectionValue" begin
+    Random.seed!(2)
+    n, N, alpha = 10, 10^7, 0.05
+    mActual = 0.75
+    dist0 = Uniform(0,1)
+    empiricalDistUnderH0 = [ts(rand(dist0,n)) for _ in 1:N]
+    rejectionValue = quantile(empiricalDistUnderH0,alpha)
+    rejectionValue = round(rejectionValue,digits = 2)
+    @test rejectionValue == 0.61
+    end
 
-sample = rand(dist1,n)
-testStat = ts(sample)
-pValue = sum(empiricalDistUnderH0 .<= testStat) / N
-
-if testStat > rejectionValue
-    print("Didn't reject: ", round(testStat,digits = 4))
-    print(" > ", round(rejectionValue,digits = 4))
-else
-    print("Reject: ", round(testStat,digits = 4))
-    print(" <= ", round(rejectionValue,digits = 4))
+@testset "pValue" begin
+    Random.seed!(2)
+    n, N, alpha = 10, 10^7, 0.05
+    mActual = 0.75
+    dist0 = Uniform(0,1)
+    empiricalDistUnderH0 = [ts(rand(dist0,n)) for _ in 1:N]
+    rejectionValue = quantile(empiricalDistUnderH0,alpha)
+    dist1 = Uniform(0,mActual)
+    sample = rand(dist1,n)
+    testStat = ts(sample)
+    pValue = sum(empiricalDistUnderH0 .<= testStat) / N
+    pValue = round(pValue,digits = 2)
+    @test pValue == 0.01
 end
-println("\np-value = $(round(pValue,digits = 4))")
 
-stephist(empiricalDistUnderH0, bins = 100, c = :blue, normed = true, label = "")
-plot!([testStat, testStat], [0, 4], c = :red, label = "Observed test statistic")
-plot!([rejectionValue, rejectionValue], [0, 4], c = :black, ls = :dash,
-    label = "Critical value boundary", legend = :topleft, ylims = (0,4),
-        xlabel = "x", ylabel = "Density")
+@testset "end_to_end" begin
+    Random.seed!(2)
+    n, N, alpha = 10, 10^7, 0.05
+    mActual = 0.75
+    dist0 = Uniform(0,1)
+    empiricalDistUnderH0 = [ts(rand(dist0,n)) for _ in 1:N]
+    rejectionValue = quantile(empiricalDistUnderH0,alpha)
+    dist1 = Uniform(0,mActual)
+    sample = rand(dist1,n)
+    testStat = ts(sample)
+    pValue = sum(empiricalDistUnderH0 .<= testStat) / N
+
+    if testStat > rejectionValue
+        print("Didn't reject: ", round(testStat,digits = 4))
+        print(" > ", round(rejectionValue,digits = 4))
+    else
+        print("Reject: ", round(testStat,digits = 4))
+        print(" <= ", round(rejectionValue,digits = 4))
+    end
+    println("\np-value = $(round(pValue,digits = 4))")
+
+    stephist(empiricalDistUnderH0, bins = 100, c = :blue, normed = true, label = "")
+    plot!([testStat, testStat], [0, 4], c = :red, label = "Observed test statistic")
+    plot!([rejectionValue, rejectionValue], [0, 4], c = :black, ls = :dash,    label = "Critical value boundary", legend = :topleft, ylims = (0,4),        xlabel = "x", ylabel = "Density")
+end
