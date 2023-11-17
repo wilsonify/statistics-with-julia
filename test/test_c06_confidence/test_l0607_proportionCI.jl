38cdbbfccef4aa7092576a97b7e8cf140b50ae3e
wilsonify <tom.andrew.wilson@gmail.com>
2023-11-17 08:12:02 -0500

    more ch06 tests

--- test/test_c06_confidence/test_l0607_proportionCI.jl
+++ test/test_c06_confidence/test_l0607_proportionCI.jl
@@ -2,28 +2,54 @@ using CSV, DataFrames, CategoricalArrays, DataFrames, Distributions
 path_to_here = @__DIR__
 path_to_data = abspath("$path_to_here/../../data")
 
-data = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
-println("Levels of Grade: ", levels(data.Grade))
-println("Data points: ", nrow(data))
+read_purchaseData() = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
+function sum_of_missing(arr)
+    is_missing  = ismissing.(arr)
+    result = sum(.!(is_missing))
+    return result
+    end
 
-n = sum(.!(ismissing.(data.Grade)))
-println("Non-missing data points: ", n)
+using Test
+@testset "read_purchaseData" begin
+data = read_purchaseData()
+@test levels(data.Grade) == ["A", "B", "C", "D", "E"]
+@test nrow(data) == 200
+end
+
+@testset "sum_ismissing" begin
+data = read_purchaseData()
+n = sum_of_missing(data.Grade)
+@test n == 187
+end
+
+@testset "" begin
+data = read_purchaseData()
+n = sum_of_missing(data.Grade)
 data2 = dropmissing(data[:,[:Grade]],:Grade)
-
 gradeInQuestion = "E"
 indicatorVector = data2.Grade .== gradeInQuestion
 numSuccess = sum(indicatorVector)
 phat = numSuccess/n
 serr = sqrt(phat*(1-phat)/n)
+@test gradeInQuestion == "E"
+@test numSuccess == 61
+@test round(phat,digits=2)==0.33
+end
 
+@testset "" begin
+data = read_purchaseData()
+n = sum_of_missing(data.Grade)
+data2 = dropmissing(data[:,[:Grade]],:Grade)
+gradeInQuestion = "E"
+indicatorVector = data2.Grade .== gradeInQuestion
+numSuccess = sum(indicatorVector)
+phat = numSuccess/n
+serr = sqrt(phat*(1-phat)/n)
 alpha = 0.05
 confidencePercent = 100*(1-alpha)
 zVal = quantile(Normal(),1-alpha/2)
 confInt = (phat - zVal*serr, phat + zVal*serr)
+confInt = round.(confInt,digits=2)
+@test confInt ==  (0.26, 0.39)
+end
 
-println("\nOut of $n non-missing observations, "*
-        "$numSuccess are at level $gradeInQuestion.")
-println("Hence a point estimate for the proportion "*
-        "of grades at level $gradeInQuestion is $phat.")
-println("A $confidencePercent% confidence interval for "*
-        "the proprotion of level $gradeInQuestion is:\n$confInt.")
--- test/test_c06_confidence/test_l0608_proportionCIsampleSize.jl
+++ test/test_c06_confidence/test_l0608_proportionCIsampleSize.jl
@@ -1,4 +1,18 @@
+#!
+# You want to plan an experiment or a sampling scheme such that margin of error, E, is not too wide.
+# For example, “not more than 0.1”. For this, you need to choose a sample size n prior to sampling.
+!#
+using Test
+
+proportions_sample_size_formula(eps) = ceil(1 / eps^2)
+
 for eps in [0.1, 0.05, 0.02, 0.01]
-    n = ceil(1/eps^2)
-    println("For epsilon = $eps set n = $n")
+    println("For epsilon = $eps")
+    n = proportions_sample_size_formula(eps)
+    println("set n = $n")
+end
+
+@testset "" begin
+    n = proportions_sample_size_formula(0.02)
+    @test n == 2500
 end
