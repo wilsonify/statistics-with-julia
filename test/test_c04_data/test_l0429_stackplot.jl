d3a2b659f0d3c954eca3b23d5bc25f44ca1bf2e3
wilsonify <tom.andrew.wilson@gmail.com>
2023-11-20 08:32:35 -0500

    better test 0429

--- test/test_c04_data/test_l0429_stackplot.jl
+++ test/test_c04_data/test_l0429_stackplot.jl
@@ -1,8 +1,26 @@
 using DataFrames, CSV, CategoricalArrays, Plots;
 path_to_here = @__DIR__
-path_to_data = abspath("$path_to_here/../data")
+path_to_data = abspath("$path_to_here/../../data")
 
 read_companyData() = CSV.read("$path_to_data/companyData.csv", DataFrame)
+
+using Test
+@testset "stackplot test" begin
+    df = read_companyData()
+    @test nrow(df) == 15
+end
+
+@testset "year levels test" begin
+    df = read_companyData()
+    years = levels(df.Year)
+    @test years == [2012, 2013, 2014, 2015, 2016]
+end
+
+@testset "mktCap test" begin
+    df = read_companyData()
+    mktCap = reshape(df.MarketCap, 5, 3)
+    @test length(mktCap)==15
+end
 
 @testset "stackplot test" begin
     df = read_companyData()
