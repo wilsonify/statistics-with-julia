c27c0989efa7a506747b02de57ec200d2e08e712
wilsonify <tom.a.wilson@gmail.com>
2023-12-18 17:46:57 -0500

    add comments to 0333 0334

--- src/c03_distribution/l0333_multiDimensionalIntegration.jl
+++ src/c03_distribution/l0333_multiDimensionalIntegration.jl
@@ -1,22 +1,21 @@
-# Multidimensional integration
+# Multidimensional integration using the HCubature package
 using HCubature
+# The function definition
+f(x) = (2 * pi)^(-length(x) / 2) * exp(-(1/2)*x'x)
 
-#=
-We use the HCubature package. In line 3, we deﬁne M. Then the integration is performed over a square
-of width twice of M, centered at the origin. In line 4, we deﬁne maxD as the number of dimensions up
-to which we wish to carry out integration. The function deﬁnition in line 6 implements (3.32). We
-loop over the dimensions in lines 8–13, each time computing the integral in line 11 where we specify
-maxevals as the maximum number of evaluations. The result is a tuple of the integral value and error,
-which are assigned to I and e, respectively.
-=#
 function main_l0333_multiDimensionalIntegration()
+    # define M
     M = 4.5
+    # define the number of dimensions to integrate up to.
     maxD = 10
-    f(x) = (2 * pi)^(-length(x) / 2) * exp(-(1/2)*x'x)
+    # integrate over a square of width twice of M, centered at the origin.
     for n in 1:maxD
+        #loop over the dimensions, each time computing the integral
         a = -M*ones(n)
         b = M*ones(n)
+        # specify the maximum number of evaluations.
         I,e = hcubature(f, a, b, maxevals = 10^7)
+        # The result is a tuple of the integral value (I) and error (e).
         println("n = $(n), integral = $(I), error (estimate) = $(e)")
     end
 end
