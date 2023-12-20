464414f4dc42bd8b9c2119731e80240360019b5e
wilsonify <tom.a.wilson@gmail.com>
2023-12-19 21:05:34 -0500

    add tests for 0333 0334

--- src/c03_distribution/l0333_multiDimensionalIntegration.jl
+++ src/c03_distribution/l0333_multiDimensionalIntegration.jl
@@ -20,19 +20,3 @@ function main_l0333_multiDimensionalIntegration()
     end
 end
 
-# Multidimensional integration
-using HCubature
-using Test
-@testset "end_to_end" begin
-M = 4.5
-maxD = 10
-
-f(x) = (2 * pi)^(-length(x) / 2) * exp(-(1/2)*x'x)
-
-for n in 1:maxD
-    a = -M*ones(n)
-    b = M*ones(n)
-    I,e = hcubature(f, a, b, maxevals = 10^7)
-    println("n = $(n), integral = $(I), error (estimate) = $(e)")
-end
-end
