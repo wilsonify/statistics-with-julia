3d014413b94479b801e56815f1fd9fc4020c7370
wilsonify <tom.andrew.wilson@gmail.com>
2023-12-24 12:12:49 -0500

    l0408_dataframeReshape.jl

--- src/c04_data/l0408_dataframeReshape.jl
+++ src/c04_data/l0408_dataframeReshape.jl
@@ -1,72 +1,101 @@
 # Reshaping, joining and merging data frames
-#=
-In line 2,
-we create data in the same manner as the previous listings.
-In lines 4–6, we create three separate data frames.
-The first, newCol, consists of a single column :Validated with the same number of rows as data.
-The second, newRow, consists of two rows with :Name and :PhoneNumber columns.
-The third, newData, has two rows and two columns, :Name and :Job.
-In line 9,
-hcat() is used to horizontally concatenate newCol to data.
-In line 12, vcat() is used to vertically concatenate data and newRow,
-with the new row appended to the bottom of the data frame.
-Note cols=:union is used so that all columns from both data frames are kept,
-and missing entries recorded where applicable.
-Alternatively, :equal or :intersect could have been used, or an array of columns to be kept instead.
-In line 15,
-join() is used to join data and newData together, based on the :Name column.
-Note that join() can be used in several different ways.
-The functions select!() and unique!() are demonstrated in lines 18–22.
-Another function not shown here is stack(),
-which can be used to stack a data frame from a wide format to a long format.
-We recommend the reader consult the DataFrames manual
-for further information on each of the functions listed here.
-=#
-using DataFrames, CSV
-path_to_here = @__DIR__
-path_to_data = abspath("$path_to_here/../../data")
 
-data = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
-
-newCol = DataFrame(Validated = ones(Int, size(data,1)))
-newRow = DataFrame([["JOHN", "JACK"] [123456, 909595]], [:Name, :PhoneNo])
-newData = DataFrame(Name = ["JOHN", "ASHELY", "MARYANNA"],
-                    Job = ["Lawyer", "Doctor", "Lawyer"])
-
-data = hcat(data, newCol)
-println(first(data, 3), "\n")
-
-data = vcat(data, newRow, cols = :union)
-println(last(data, 3), "\n")
-
-#data = leftjoin(data, newData, on=:Name)
-data = leftjoin(data, newData, on = :Name, matchmissing = :notequal)
-
-
-println(data, "\n")
-
-select!(data,[:Name, :Job])
-println(data, "\n")
-
-unique!(data,:Job)
-println(data)
-
-using DataFrames, CSV
-
-path_to_here = @__DIR__
-include("$path_to_here/t01_are_dataframes_equal.jl")
-include("$path_to_here/t02_are_lists_equal.jl")
-include("$path_to_here/t04_dataframe_to_dict.jl")
-
-path_to_data = abspath("$path_to_here/../../data")
+using DataFrames
+using CSV
 
 read_purchaseData() = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
 
+function horizontal_concatenate(data::DataFrame, newCol::DataFrame)
+    # Check if the number of rows in data and newCol matches
+    if size(data, 1) != size(newCol, 1)
+        throw(DimensionMismatch("The number of rows in data ($(size(data, 1))) and newCol $(size(newCol, 1)) must match"))
+    end
+
+    # Check if the columns in data and newCol have the same names
+    if any(isequal(names(data), names(newCol)))
+        println("Columns in data and newCol same names.")
+    end
+
+    # Perform horizontal concatenation
+    result = hcat(data, newCol)
+
+    return result
+end
+
+function join_on_name(data, newData)
+    # join data and newData together, based on the :Name column.
+    # Note that join() can be used in several different ways.
+    # innerjoin: the output contains rows for values of the key that exist in all passed data frames.
+    # leftjoin: the output contains rows for values of the key that exist in the first (left) argument,
+    # whether or not that value exists in the second (right) argument.
+    # rightjoin: the output contains rows for values of the key that exist in the second (right) argument,
+    # whether or not that value exists in the first (left) argument.
+    # outerjoin: the output contains rows for values of the key that exist in any of the passed data frames.
+    # semijoin: Like an inner join, but output is restricted to columns from the first (left) argument.
+    # antijoin: The output contains rows for values of the key that exist in the first (left) but not the second (right) argument.
+    # As with semijoin, output is restricted to columns from the first (left) argument.
+    # crossjoin: The output is the cartesian product of rows from all passed data frames.
+    return leftjoin(data, newData, on = :Name, matchmissing = :notequal)
+end
+
+function main_l0408_dataframeReshape()
+    path_to_here = @__DIR__
+    path_to_data = abspath("$path_to_here/../../data")
+    # create data
+    data = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
+
+    # create three separate data frames.
+    # The first consists of a single column with the same number of rows as data.
+    newCol = DataFrame(Validated = ones(Int, size(data,1)))
+    # The second consists of two rows with :Name and :PhoneNumber columns.
+    newRow = DataFrame([["JOHN", "JACK"] [123456, 909595]], [:Name, :PhoneNo])
+    # The third has two rows and two columns, :Name and :Job.
+    newData = DataFrame(Name = ["JOHN", "ASHELY", "MARYANNA"], Job = ["Lawyer", "Doctor", "Lawyer"])
+
+    # horizontally concatenate newCol to data.
+    data = horizontal_concatenate(data, newCol)
+    println(first(data, 3))
+
+    # vertically concatenate newRow, with the new row appended to the bottom of the data frame.
+    data = vcat(data, newRow, cols = :union)
+    println(last(data, 3), "\n")
+
+    # join data and newData together, based on the :Name column.
+    # Note that join() can be used in several different ways.
+    data = join_on_name(data, newData)
+    println(data, "\n")
+
+    # The functions select!() and unique!() are demonstrated.
+    select!(data,[:Name, :Job])
+    println(data, "\n")
+
+    unique!(data,:Job)
+    println(data)
+    # Another function not shown here is stack(),
+    # which can be used to stack a data frame from a wide format to a long format.
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main_l0408_dataframeReshape()
+end
+
+using DataFrames
+using CSV
+
+path_to_here = @__DIR__
+include("$path_to_here/../t01_testing/t01_are_dataframes_equal.jl")
+include("$path_to_here/../t01_testing/t02_are_lists_equal.jl")
+include("$path_to_here/../t01_testing/t04_dataframe_to_dict.jl")
+
+path_to_data = abspath("$path_to_here/../../data")
+
+
+
 using Test
-@testset "hcat test" begin
-    data = read_purchaseData()
+@testset "hcat axis=1 test" begin
+    data = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
     newCol = DataFrame(Validated = ones(Int, size(data, 1)))
-    data = hcat(data, newCol)
+    data = horizontal_concatenate(data, newCol)
     expected_data = DataFrame(Dict{Symbol, Vector{Any}}(:Grade => ["A", "B", "E"],
         :Price => [79700, missing, 24311],
         :Date => ["14/09/2008", "11/03/2008", "5/08/2008"],
@@ -75,8 +104,8 @@ using Test
     @test are_dataframes_equal(first(data, 3), expected_data)
 end
 
-@testset "vcat test" begin
-    data = read_purchaseData()
+@testset "vcat axis=0 test" begin
+    data = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
     newRow = DataFrame([["JOHN", "JACK"] [123456, 909595]], [:Name, :PhoneNo])
     newData = DataFrame(Name = ["JOHN", "ASHELY", "MARYANNA"], Job = ["Lawyer", "Doctor", "Lawyer"])
     data = vcat(data, newRow, cols = :union)
@@ -89,9 +118,9 @@ end
 end
 
 @testset "leftjoin test" begin
-    data = read_purchaseData()
+    data = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
     newData = DataFrame(Name = ["JOHN", "ASHELY", "MARYANNA"], Job = ["Lawyer", "Doctor", "Lawyer"])
-    result = leftjoin(data, newData, on = :Name, matchmissing = :notequal)
+    result = join_on_name(data, newData)
     expected_data = DataFrame(Grade = ["A", "E", "B"],
         Price = [79700, 24311, missing],
         Date = ["14/09/2008", "5/08/2008", "11/03/2008"],
@@ -101,10 +130,9 @@ end
 end
 
 @testset "select test" begin
-    data = read_purchaseData()
+    data = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
     newData = DataFrame(Name = ["JOHN", "ASHELY", "MARYANNA"], Job = ["Lawyer", "Doctor", "Lawyer"])
-    result = leftjoin(data, newData, on = :Name, matchmissing = :notequal)
-
+    result = join_on_name(data, newData)
     select!(result, [:Name, :Job])
     expected_data = DataFrame(Grade = ["A", "B", "E"],
         Price = [79700, missing, 24311],
@@ -114,9 +142,9 @@ end
 end
 
 @testset "unique test" begin
-    data = read_purchaseData()
+    data = CSV.read("$path_to_data/purchaseData.csv", DataFrame, copycols = true)
     newData = DataFrame(Name = ["JOHN", "ASHELY", "MARYANNA"], Job = ["Lawyer", "Doctor", "Lawyer"])
-    result = leftjoin(data, newData, on = :Name, matchmissing = :notequal)
+    result = join_on_name(data, newData)
     unique!(result, :Job)
     expected_data = DataFrame(Grade = ["A", "B", "E"],
         Price = [79700, missing, 24311],
