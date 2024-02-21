0dd48a48cadeb7b4d80f20a846a24fe840364dd8
wilsonify <tom.andrew.wilson@gmail.com>
2024-02-21 08:24:16 -0500

    test_l0520_bayesMCMC.jl

--- src/c05_inference/tests/test_l0520_bayesMCMC.jl
+++ src/c05_inference/tests/test_l0520_bayesMCMC.jl
@@ -1,10 +1,19 @@
+using Test
+using c05_inference: sampler
+using c05_inference: prior3_gamma
+using c05_inference: like3_poisson
+using c05_inference: posteriorUpToK3
+using c05_inference: foldedNormalPDF
+using c05_inference: foldedNormalRV
+using c05_inference: closedFormPosterior
 
 @testset "end_to_end" begin
-    mcmcSamples = sampler(posteriorUpToK,foldedNormalPDF,foldedNormalRV)
+    mcmcSamples = sampler(posteriorUpToK3,foldedNormalPDF,foldedNormalRV)
     result = mean(mcmcSamples)
     result = round(result,digits = 2)
     @test result == 2.07
+    lamRange = 0:0.01:10
     stephist(mcmcSamples, bins = 100,    c = :black, normed = true, label = "Histogram of MCMC samples")
-    plot!(lamRange, prior.(lamRange),     c = :blue, label = "Prior distribution")
+    plot!(lamRange, prior3_gamma.(lamRange),     c = :blue, label = "Prior distribution")
     plot!(lamRange, closedFormPosterior.(lamRange),     c = :red, label = "Posterior distribution",    xlims = (0, 10), ylims = (0, 1.2),    xlabel = L"\lambda",ylabel = "Density")
 end
