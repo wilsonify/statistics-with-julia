ba98b0fa4c7c7370e1a2a1c49a58c95f1d0b0b57
wilsonify <tom.andrew.wilson@gmail.com>
2024-02-22 08:31:55 -0500

    test_l0605_twoPopMVUnknownAUnequal.jl

--- src/c06_confidence/src/l0605_twoPopMVUnknownAUnequal.jl
+++ src/c06_confidence/src/l0605_twoPopMVUnknownAUnequal.jl
@@ -1,21 +1,57 @@
 #=
-
+Confidence Interval for a difference in means
+with unknown and unequal variance
 =#
-using CSV, Distributions, HypothesisTests, DataFrames
-path_to_here = @__DIR__
-path_to_data = abspath("$path_to_here/../../data")
+using CSV
+using Distributions
+using HypothesisTests
+using DataFrames
+using io_library: read_machine_data
 
-data1 = CSV.read("$path_to_data/machine1.csv", DataFrame, header=false)[:,1]
-data2 = CSV.read("$path_to_data/machine2.csv", DataFrame, header=false)[:,1]
-xBar1, xBar2 = mean(data1), mean(data2)
-s1, s2 = std(data1), std(data2)
-n1, n2 = length(data1), length(data2)
-alpha = 0.05
+function confidence_interval_unknown_unequal_var_formula(data1, data2)
+    alpha = 0.05
+    xBar1, xBar2 = mean(data1), mean(data2)
+    s1, s2 = std(data1), std(data2)
+    n1, n2 = length(data1), length(data2)
+    # calculate degrees of freedom
+    v = (s1^2 / n1 + s2^2 / n2)^2 / ( (s1^2 / n1)^2 / (n1 - 1) + (s2^2 / n2)^2 / (n2 - 1) )
+    # derive the T-statistic
+    t = quantile(TDist(v),1 - alpha / 2)
+    lower = xBar1 - xBar2 - t*sqrt(s1^2 / n1 + s2^2 / n2)
+    upper = xBar1 - xBar2 + t*sqrt(s1^2 / n1 + s2^2 / n2)
+    return (lower, upper)
+end
 
-v = (s1^2/n1 + s2^2/n2)^2 / ( (s1^2/n1)^2 / (n1-1) + (s2^2/n2)^2 / (n2-1) )
+function confidence_interval_unknown_unequal_var_builtin(data1, data2)
+    alpha = 0.05
+    unequal_variance_t_test = UnequalVarianceTTest(data1,data2)
+    ci = confint(unequal_variance_t_test,level = 1 - alpha,tail = :both)
+    return ci
+end
 
-t = quantile(TDist(v),1-alpha/2)
+function main_l0605_twoPopMVUnknownAUnequal()
+    data1 = CSV.read("$(@__DIR__)/../../data/machine1.csv", DataFrame, header = false)[:,1]
+    data2 = CSV.read("$(@__DIR__)/../../data/machine2.csv", DataFrame, header = false)[:,1]
+    xBar1, xBar2 = mean(data1), mean(data2)
+    s1, s2 = std(data1), std(data2)
+    n1, n2 = length(data1), length(data2)
+    alpha = 0.05
 
-println("Calculating formula: ", (xBar1 - xBar2 - t*sqrt(s1^2/n1 + s2^2/n2),
-				  xBar1 - xBar2 + t*sqrt(s1^2/n1 + s2^2/n2)))
-println("Using confint(): ",   confint(UnequalVarianceTTest(data1,data2),level=1-alpha,tail = :both))
+    # calculate degrees of freedom
+    v = (s1^2 / n1 + s2^2 / n2)^2 / ( (s1^2 / n1)^2 / (n1 - 1) + (s2^2 / n2)^2 / (n2 - 1) )
+
+    # derive the T-statistic
+    t = quantile(TDist(v),1 - alpha / 2)
+
+    # evaluate confidence interval
+    lower = xBar1 - xBar2 - t*sqrt(s1^2 / n1 + s2^2 / n2)
+    upper = xBar1 - xBar2 + t*sqrt(s1^2 / n1 + s2^2 / n2)
+    println("Calculating formula: ", (lower,upper))
+
+    # compare to the builtin confint()
+    unequal_variance_t_test = UnequalVarianceTTest(data1,data2)
+    ci = confint(unequal_variance_t_test,level = 1 - alpha,tail = :both)
+    println("Using confint(): ",   )
+end
+
+
--- src/c06_confidence/tests/test_l0605_twoPopMVUnknownAUnequal.jl
+++ src/c06_confidence/tests/test_l0605_twoPopMVUnknownAUnequal.jl
@@ -1,52 +1,31 @@
-using CSV, Distributions, HypothesisTests, DataFrames
-
-path_to_here = @__DIR__
-path_to_data = abspath("$path_to_here/../../../data")
-
-read_machine(n) = CSV.read("$path_to_data/machine$n.csv", DataFrame, header = false)[:,1]
-
-function confidence_interval_formula(data1, data2)
-    alpha = 0.05
-    xBar1, xBar2 = mean(data1), mean(data2)
-    s1, s2 = std(data1), std(data2)
-    n1, n2 = length(data1), length(data2)
-    v = (s1^2 / n1 + s2^2 / n2)^2 / ( (s1^2 / n1)^2 / (n1 - 1) + (s2^2 / n2)^2 / (n2 - 1) )
-    t = quantile(TDist(v),1 - alpha / 2)
-    lower = xBar1 - xBar2 - t*sqrt(s1^2 / n1 + s2^2 / n2)
-    upper = xBar1 - xBar2 + t*sqrt(s1^2 / n1 + s2^2 / n2)
-    return (lower, upper)
-    end
-
-function confidence_interval_builtin(data1, data2)
-    alpha = 0.05
-    return confint(UnequalVarianceTTest(data1,data2),level = 1 - alpha,tail = :both)
-    end
-
 using Test
+using c06_confidence: confidence_interval_unknown_unequal_var_formula
+using c06_confidence: confidence_interval_unknown_unequal_var_builtin
+
 @testset "read_machine" begin
-    data = read_machine("1")
+    data = read_machine_data("$(@__DIR__)/../../../data/machine1.csv")[:,1]
     @test length(data) == 20
 end
 
-@testset "confidence_interval_formula" begin
-    data1 = read_machine("1")
-    data2 = read_machine("2")
-    result = confidence_interval_formula(data1,data2)
+@testset "confidence_interval_unknown_unequal_var_formula" begin
+    data1 = read_machine_data("$(@__DIR__)/../../../data/machine1.csv")[:,1]
+    data2 = read_machine_data("$(@__DIR__)/../../../data/machine2.csv")[:,1]
+    result = confidence_interval_unknown_unequal_var_formula(data1,data2)
     result = round.(result,digits = 2)
     @test result == (1.1, 2.92)
 end
 
-@testset "confidence_interval_builtin" begin
-    data1 = read_machine("1")
-    data2 = read_machine("2")
-    result = confidence_interval_builtin(data1, data2)
+@testset "confidence_interval_unknown_unequal_var_builtin" begin
+    data1 = read_machine_data("$(@__DIR__)/../../../data/machine1.csv")[:,1]
+    data2 = read_machine_data("$(@__DIR__)/../../../data/machine2.csv")[:,1]
+    result = confidence_interval_unknown_unequal_var_builtin(data1, data2)
     result = round.(result,digits = 2)
     @test result == (1.1, 2.92)
 end
 
 @testset "matching" begin
-    data1 = read_machine("1")
-    data2 = read_machine("2")
+    data1 = read_machine_data("$(@__DIR__)/../../../data/machine1.csv")[:,1]
+    data2 = read_machine_data("$(@__DIR__)/../../../data/machine2.csv")[:,1]
     cif = confidence_interval_formula(data1,data2)
     cif = round.(cif,digits = 2)
     cib = confidence_interval_builtin(data1,data2)
